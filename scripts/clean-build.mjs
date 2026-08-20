ca39ae09c4208b5e5a6451e47a67f44a8554e903
Dparamet <dparamet1@gmail.com>
2026-08-20 10:45:18 +0700

    ux/ui:make ux more responsive and easy to use

--- scripts/clean-build.mjs
+++ scripts/clean-build.mjs
@@ -2,6 +2,6 @@ import { rm } from "node:fs/promises";
 import { resolve } from "node:path";
 
 await Promise.all([
-  rm(resolve("dist"), { force: true, recursive: true }),
-  rm(resolve("out"), { force: true, recursive: true }),
+  rm(resolve("dist"), { force: true, recursive: true, maxRetries: 3 }).catch(() => {}),
+  rm(resolve("out"), { force: true, recursive: true, maxRetries: 3 }).catch(() => {}),
 ]);
