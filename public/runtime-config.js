ade0e8b8fe12fe56ede9b15014360de060e7440f
Dparamet <dparamet1@gmail.com>
2026-09-24 12:35:34 +0700

    chore: set patient API URL to production

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -1,5 +1,5 @@
 window.PATIENT_APP_ENV = {
-  "API_BASE_URL": "http://127.0.0.1:8000",
+  "API_BASE_URL": "https://hospital.bfirstkok.me",
   "STATUS_REFRESH_MS": 10000,
   "GOOGLE_CLIENT_ID": "477244523835-vopgha7b47cohjn2ouk80ng1gsup5tl8.apps.googleusercontent.com"
 };
